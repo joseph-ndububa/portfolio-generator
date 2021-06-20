5e746bbc1d41b43a7772e05f21a1a35663638387
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-06-19 21:45:50 -0400

    modified html template to handle more data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 //const [name, github] = profileDataArgs;
 
@@ -117,5 +117,10 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
   });
